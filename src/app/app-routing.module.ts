41359ddc1441669d0bc58e1654a1a1127055c50c
Victor.Aguiar <victor.aguiar@cingo.com.br>
2024-10-01 17:52:58 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountdownComponent } from './countdown/components/countdown/countdown.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CountdownComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
