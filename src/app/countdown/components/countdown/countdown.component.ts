f37774c4a4fdd705583a21bcfe82b21f3c39f6a7
Victor.Aguiar <victor.aguiar@cingo.com.br>
2024-10-01 17:49:13 -0300

    angular cdk e countdown initial

--- src/app/countdown/components/countdown/countdown.component.ts
+++ src/app/countdown/components/countdown/countdown.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
-  styleUrl: './countdown.component.scss'
+  styleUrls: ['./countdown.component.css']
 })
-export class CountdownComponent {
+export class CountdownComponent implements OnInit, OnDestroy {
 
-}
+  days: string = '';
+
+  hours: string = '';
+
+  minutes: string = '';
+
+  seconds: string = '';
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.countDown();
+  }
+  ngOnDestroy(): void {
+    
+  }
+
+  countDown()
+  {
+    const targetDate = new Date('October 19, 2024 18:00:00').getTime();
+
+    const countdown = setInterval(() => {
+
+        const now = new Date().getTime();
+
+        const timeDifference = targetDate - now;
+
+        const days = Math.floor(timeDifference / (86400000));
+        const hours = Math.floor((timeDifference % (86400000)) / (3600000));
+        const minutes = Math.floor((timeDifference % (3600000)) / (60000));
+        const seconds = Math.floor((timeDifference % (60000)) / 1000);
+        this.days = days >= 10 ? `${days}` : `0${days}`
+        this.hours = hours >= 10 ? `${hours}` : `0${hours}`
+        this.minutes = minutes >= 10 ? `${minutes}` : `0${minutes}`
+        this.seconds = seconds >= 10 ? `${seconds}` : `0${seconds}`
+
+        if (timeDifference < 0) {
+            clearInterval(countdown);
+        }
+    }, 1000);
+
+  }
+ }
